e4dd927e991e67c5bd5dc5d7377394d04d1782f3
JuanJoseAriasJaramillo <juanjoseariasj@hotmail.com>
2021-07-07 10:24:02 -0500

    Rehago el script DecisionScript sin lograr solucionar nada

--- Assets/Scripts/DecisionScript.cs
+++ Assets/Scripts/DecisionScript.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class DecisionScript : MonoBehaviour
 {
     public bool willItRainToday = false;
-    // Start is called before the first frame update
+     // Start is called before the first frame update
     void Start()
     {
         if (willItRainToday)
         {
-            Debug.Log("No te olvides coger el paraguas");
+            Debug.Log("No te olvides de coger el paraguas");
+
         }
         else
         {
-            Debug.Log("No lo cojas, que hjace muchjo calor");
+            Debug.Log("No lo cojas hombre, que hace mucho sol");
         }
     }
 
